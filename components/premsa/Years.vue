b82e68e1edcc62ef891c70f8d67efc5d0e27952b
marcurto <mcurtogilabert@gmail.com>
2021-02-15 16:42:53 +0100

    condicional rendering premsa

--- components/premsa/Years.vue
+++ components/premsa/Years.vue
@@ -1,30 +1,46 @@
 <template>
   <nav class="years">
     <UiList theme="horizontal" class="years__list">
-      <li>2018</li>
-      <li>2019</li>
-      <li>2020</li>
-      <li>2021</li>
+      <li @click="toggleComponent18">2018</li>
+      <li @click="toggleComponent19">2019</li>
+      <li @click="toggleComponent20">2020</li>
+      <li @click="toggleComponent21">2021</li>
     </UiList>
   </nav>
 </template>
 
 <script>
 import UiList from "@/components/ui/objects/UiList.vue";
+import RecullNoticies21 from "@/components/premsa/RecullNoticies21.vue";
+import RecullNoticies20 from "@/components/premsa/RecullNoticies20.vue";
+import RecullNoticies19 from "@/components/premsa/RecullNoticies19.vue";
+import RecullNoticies18 from "@/components/premsa/RecullNoticies18.vue";
+
 
 export default {
   components: {
     UiList,
   },
-  methods: {
-    move(route) {
-      this.$props.callback();
-      this.$router.push(route);
-    },
+ data() {
+    return {
+      showComponent18: false,
+      showComponent19: false,
+      showComponent20: false,
+      showComponent21: true
+    };
   },
-  props: {
-    callback: {
-      required: true,
+  methods: {
+    toggleComponent18 () {
+      this.showComponent18 = !this.showComponent18
+    },
+    toggleComponent19 () {
+      this.showComponent19 = !this.showComponent19
+    },
+    toggleComponent20 () {
+      this.showComponent20 = !this.showComponent20
+    },
+    toggleComponent21 () {
+      this.showComponent21 = !this.showComponent21
     },
   },
 };
--- pages/premsa.vue
+++ pages/premsa.vue
@@ -5,11 +5,18 @@
         <Cover title="SALA DE PREMSA" />
       </Overally>
     </CoverBackground>
-    <Years />
-    <!-- <RecullNoticies18 /> -->
-    <!-- <RecullNoticies19 /> -->
-    <!-- <RecullNoticies20 /> -->
-    <RecullNoticies21 />
+     <nav class="years">
+    <UiList theme="horizontal" class="years__list">
+      <li @click="toggleComponent = '18'">2018</li>
+      <li @click="toggleComponent = '19'">2019</li>
+      <li @click="toggleComponent = '20'">2020</li>
+      <li @click="toggleComponent = '21'">2021</li>
+    </UiList>
+  </nav>
+    <RecullNoticies18 v-if="toggleComponent === '18'"/>
+    <RecullNoticies19 v-if="toggleComponent === '19'"/>
+    <RecullNoticies20 v-if="toggleComponent === '20'" />
+    <RecullNoticies21 v-if="toggleComponent === '21'"/>
     <Footer />
   </section>
 </template>
@@ -18,8 +25,10 @@
 import CoverBackground from "@/components/ui/atoms/CoverBackground.vue";
 import Cover from "@/components/ui/molecules/Cover.vue";
 import Overally from "@/components/ui/atoms/Overally.vue";
-import Years from "@/components/premsa/Years.vue";
 import RecullNoticies21 from "@/components/premsa/RecullNoticies21.vue";
+import RecullNoticies20 from "@/components/premsa/RecullNoticies20.vue";
+import RecullNoticies19 from "@/components/premsa/RecullNoticies19.vue";
+import RecullNoticies18 from "@/components/premsa/RecullNoticies18.vue";
 import Footer from "@/components/common/Footer.vue";
 
 export default {
@@ -28,10 +37,17 @@ export default {
     CoverBackground,
     Cover,
     Overally,
-    Years,
     RecullNoticies21,
+    RecullNoticies20,
+    RecullNoticies19,
+    RecullNoticies18,
     Footer,
   },
+  data() {
+    return {
+      toggleComponent: '21'
+    };
+  }
 };
 </script>
 
@@ -43,5 +59,21 @@ export default {
 section {
   height: 100%;
   color: variables.$title-blue;
+}
+
+
+.years {
+  width: 100%;
+  margin: 4rem 0rem;
+
+  &__list{
+    margin-right: 4rem;
+    border-bottom: black 2px solid;
+    justify-content: flex-end;
+
+    li{
+      margin-right: 1rem;
+    }
+  }
 }
 </style>
